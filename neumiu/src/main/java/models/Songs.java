89941878e244648b80d98e8f4469a8e4980427b4
ChrisLeOtaku <christopherfarrant1998@gmail.com>
2018-02-26 13:47:23 -0700

    Changed FilePath

--- neumiu/src/main/java/models/Songs.java
+++
@@ -1,9 +0,0 @@
-package models;
-
-import java.util.ArrayList;
-
-public class Songs extends ArrayList <Songs>{
-
-	private static final long serialVersionUID = 1L;
-
-}
--- src/main/java/app/NeuMiuApp.java
+++ src/main/java/app/NeuMiuApp.java
@@ -1,9 +1,17 @@
 package app;
 
-public class NeuMiuApp {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class NeuMiuApp extends Application {
 
 	public static void main(String[] args) {
-		
+		Application.launch(NeuMiuApp.class, args);
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+
 	}
 
 }
