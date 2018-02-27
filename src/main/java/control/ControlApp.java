5f8290e4c50685178dbc7e007bc80f06ba376e00
ChrisLeOtaku <christopherfarrant1998@gmail.com>
2018-02-27 14:08:38 -0700

    Tweaked FXML

--- src/main/java/control/ControlApp.java
+++ src/main/java/control/ControlApp.java
@@ -1,50 +1,5 @@
 package control;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.SeparatorMenuItem;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-
 public class ControlApp {
-
-	@FXML
-	private MenuItem AddSong;
-
-	@FXML
-	private MenuItem DeleteSong;
-
-	@FXML
-	private SeparatorMenuItem Sep1;
-
-	@FXML
-	private SeparatorMenuItem Sep2;
-
-	@FXML
-	private MenuItem About;
-
-	@FXML
-	private Font x1;
-
-	@FXML
-	private Color x2;
-
-	@FXML
-	private MenuItem NewSong;
-
-	public void AddSong(MouseEvent f) {
-
-	}
-
-	@FXML
-	private Pane nowPlaying;
-
-	@FXML
-	private Font x3;
-
-	@FXML
-	private Color x4;
 
 }
