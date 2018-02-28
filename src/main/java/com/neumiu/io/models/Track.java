bb9d8b49812b323ed2814f11e0b556c6213574b4
ChrisLeOtaku <christopherfarrant1998@gmail.com>
2018-02-27 17:50:03 -0700

    Updated Models

--- src/main/java/com/neumiu/io/models/Track.java
+++ src/main/java/com/neumiu/io/models/Track.java
@@ -1,16 +1,49 @@
 package com.neumiu.io.models;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import org.tritonus.share.sampled.file.TAudioFileFormat;
+
 public class Track {
 
 	private String title;
-	private String genere;
+	private String genre;
 	private String artist;
-	private long totalTime;
-	private String songPath;
-	private String artPath;
+	private String totalTime;
+	private File song;
+	private File artwork;
+	private final File noArtwork = new File(getClass().getClassLoader().getResource("images/NoArtwork.png").getFile());
 
-	public Track() {
+	public Track() {}
 
+	public Track(String t, String g, String artist, File song, File art) throws IOException, UnsupportedAudioFileException {
+		this.setTitle(t);
+		this.setGenre(g);
+		this.setArtist(artist);
+		this.setSongPath(song);
+		this.setArtwork(art);
+		this.setTotalTime(calcTotalTime());
+	}
+	
+	private String calcTotalTime() throws UnsupportedAudioFileException, IOException {
+		String totalRunTime = null;
+		AudioFileFormat fileFormat = AudioSystem.getAudioFileFormat(this.song);
+		if (fileFormat instanceof TAudioFileFormat) {
+			Map<?, ?> properties = ((TAudioFileFormat) fileFormat).properties();
+		    String key = "duration";
+		    Long microseconds = (Long) properties.get(key);
+		    int mili = (int) (microseconds / 1000);
+		    int sec = (mili / 1000) % 60;
+		    int min = (mili / 1000) / 60;
+		    totalRunTime = ("Runtime: " + min + ":" + sec);
+		} else {
+		    throw new UnsupportedAudioFileException();
+		}
+		return totalRunTime;
 	}
 
 	public String getTitle() {
@@ -18,15 +51,21 @@ public class Track {
 	}
 
 	public void setTitle(String title) {
+		if (title == null || title.isEmpty()) {
+			this.title = "Unknown";
+		}
 		this.title = title;
 	}
 
-	public String getGenere() {
-		return genere;
+	public String getGenre() {
+		return genre;
 	}
 
-	public void setGenere(String genere) {
-		this.genere = genere;
+	public void setGenre(String genre) {		
+		if (genre == null || genre.isEmpty()) {
+			this.genre = "Unknown";
+		}
+		this.genre = genre;
 	}
 
 	public String getArtist() {
@@ -34,31 +73,39 @@ public class Track {
 	}
 
 	public void setArtist(String artist) {
+		if (artist == null || artist.isEmpty()) {
+			this.artist = "Unknown";
+		}
 		this.artist = artist;
 	}
 
-	public long getTotalTime() {
+	public File getSongPath() {
+		return song;
+	}
+
+	public void setSongPath(File song) throws IOException {
+		if (song == null) {
+			throw new IOException("File was unable to be found!");
+		}
+		this.song = song;
+	}
+
+	public File getArtwork() {
+		return artwork;
+	}
+
+	public void setArtwork(File art) {
+		if (art == null) {
+			this.artwork = noArtwork;
+		}
+		this.artwork = art;
+	}
+
+	public String getTotalTime() {
 		return totalTime;
 	}
 
-	public void setTotalTime(long totalTime) {
+	public void setTotalTime(String totalTime) {
 		this.totalTime = totalTime;
 	}
-
-	public String getSongPath() {
-		return songPath;
-	}
-
-	public void setSongPath(String songPath) {
-		this.songPath = songPath;
-	}
-
-	public String getArtPath() {
-		return artPath;
-	}
-
-	public void setArtPath(String artPath) {
-		this.artPath = artPath;
-	}
-
 }
