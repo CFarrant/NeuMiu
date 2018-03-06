35490ea31861bd032f534ef4d8044a09ebab1579
Glutamein <36281832+Glutamein@users.noreply.github.com>
2018-03-06 13:52:56 -0700

    Help Window

--- src/main/java/com/neumiu/io/control/FXMLController.java
+++ src/main/java/com/neumiu/io/control/FXMLController.java
@@ -52,16 +52,16 @@ import main.java.goxr3plus.javastreamplayer.stream.StreamPlayerListener;
 
 public class FXMLController extends StreamPlayer implements Initializable, StreamPlayerListener {
 
-	//Controllers	
+	// Controllers
 	private TrackController track;
-	
-	//Savable Data
+
+	// Savable Data
 	private static List<Track> addedSongs;
-	
-	//ListView Controls
+
+	// ListView Controls
 	private static ObservableList<Track> songs;
-	
-	//Variables
+
+	// Variables
 	private double volumeLevel = 50;
 	private String fileName = "NeuMiu.db";
 	private boolean mute;
@@ -69,15 +69,15 @@ public class FXMLController extends StreamPlayer implements Initializable, Strea
 	private long currentPlayTime;
 	private long totalPlayTime = 0;
 	private static int currentItem = 0;
-	
-	//Temp Song Variables
+
+	// Temp Song Variables
 	private static String newSong = null;
 	private static String title = null;
 	private static String genre = null;
 	private static String artist = null;
 	private static String artwork = null;
-	
-	//GUI Elements
+
+	// GUI Elements
 	@FXML
 	private TableView<Track> songInPlaylist;
 	@FXML
@@ -91,7 +91,8 @@ public class FXMLController extends StreamPlayer implements Initializable, Strea
 	@FXML
 	private ImageView coverArt, playTrack;
 	@FXML
-	private TextField currentlyPlaying, addSongName, addSongGenre, addSongArtist, editSongName, editSongGenre, editSongArtist;
+	private TextField currentlyPlaying, addSongName, addSongGenre, addSongArtist, editSongName, editSongGenre,
+			editSongArtist;
 	@FXML
 	private Label curTime, totalTime;
 	@FXML
@@ -109,17 +110,17 @@ public class FXMLController extends StreamPlayer implements Initializable, Strea
 		songInPlaylist = new TableView<Track>();
 	}
 
-	 protected void getSongInfo(Track song) throws ClassNotFoundException, IOException {
-		 try {
-			 title = song.getTitle();
-			 newSong = song.getSongPath();
-			 artwork = song.getArtwork();
-			 artist = song.getArtist();
-		 } catch (NullPointerException e) {
-			 showAlert("Warning", null, "There were no songs to get information from!", AlertType.WARNING);
-		 }
+	protected void getSongInfo(Track song) throws ClassNotFoundException, IOException {
+		try {
+			title = song.getTitle();
+			newSong = song.getSongPath();
+			artwork = song.getArtwork();
+			artist = song.getArtist();
+		} catch (NullPointerException e) {
+			showAlert("Warning", null, "There were no songs to get information from!", AlertType.WARNING);
+		}
 	}
-	
+
 	@FXML
 	private void refresh(MouseEvent e) {
 		if (e.isPrimaryButtonDown() && e.getClickCount() == 2) {
@@ -133,13 +134,13 @@ public class FXMLController extends StreamPlayer implements Initializable, Strea
 	}
 
 	private void showAlert(String title, String header, String text, AlertType type) {
-		 Alert alert = new Alert(type);
-		 alert.setTitle(title);
-		 alert.setHeaderText(header);
-		 alert.setContentText(text);
-		 alert.showAndWait();
-	 }
-    
+		Alert alert = new Alert(type);
+		alert.setTitle(title);
+		alert.setHeaderText(header);
+		alert.setContentText(text);
+		alert.showAndWait();
+	}
+
 	@FXML
 	private void helpScreen(ActionEvent f) throws IOException {
 		Parent root = FXMLLoader.load(ClassLoader.getSystemResource("fxml/HelpWindow.fxml"));
@@ -165,7 +166,7 @@ public class FXMLController extends StreamPlayer implements Initializable, Strea
 			artwork = art.getPath();
 		}
 	}
-	
+
 	@FXML
 	private void browseMusic(ActionEvent r) throws IOException {
 		Parent root = FXMLLoader.load(ClassLoader.getSystemResource("fxml/HelpWindow.fxml"));
@@ -187,7 +188,7 @@ public class FXMLController extends StreamPlayer implements Initializable, Strea
 		Stage stage = (Stage) cancel.getScene().getWindow();
 		stage.close();
 	}
-	
+
 	@FXML
 	private void editSong(ActionEvent q) throws IOException {
 		Parent root = FXMLLoader.load(ClassLoader.getSystemResource("fxml/EditSong.fxml"));
@@ -197,16 +198,16 @@ public class FXMLController extends StreamPlayer implements Initializable, Strea
 		stage.setTitle("Edit Song");
 		stage.show();
 	}
-	
+
 	@FXML
 	private void removeSong(ActionEvent rms) {
-		try { 
+		try {
 			addedSongs.remove(currentItem);
 		} catch (IndexOutOfBoundsException ex) {
 			showAlert("Warning", null, "There are no songs to remove!", AlertType.WARNING);
 		}
 	}
-	
+
 	@FXML
 	private void addSong(ActionEvent k) throws IOException {
 		System.out.println("TEST");
@@ -216,8 +217,9 @@ public class FXMLController extends StreamPlayer implements Initializable, Strea
 		stage.setScene(scene);
 		stage.setTitle("Add New Song");
 		stage.show();
+
 	}
-	
+
 	public void saveNewSong(ActionEvent sNS) throws UnsupportedAudioFileException, IOException {
 		title = addSongName.getText();
 		artist = addSongArtist.getText();
@@ -226,7 +228,7 @@ public class FXMLController extends StreamPlayer implements Initializable, Strea
 		Stage stage = (Stage) cancel.getScene().getWindow();
 		stage.close();
 	}
-	
+
 	public void saveEditedSong(ActionEvent snsn) {
 		Track temp = addedSongs.get(currentItem);
 		addedSongs.remove(temp);
@@ -242,16 +244,16 @@ public class FXMLController extends StreamPlayer implements Initializable, Strea
 		if (!(temp.getArtwork() == artwork)) {
 			if (artwork == null) {
 				temp.setArtwork("images/NoArtwork.png");
-			}
-			else {
+			} else {
 				temp.setArtwork(artwork);
 			}
 		}
 		addedSongs.add(temp);
 		Stage stage = (Stage) cancel.getScene().getWindow();
-		stage.close();;
+		stage.close();
+		;
 	}
-	
+
 	public void mute(ActionEvent m) {
 		if (mute == true) {
 			setMute(false);
@@ -293,7 +295,7 @@ public class FXMLController extends StreamPlayer implements Initializable, Strea
 				if (song == null) {
 					showAlert("Error", null, "There is no song selected to be played!", AlertType.ERROR);
 				}
-			} 
+			}
 			if (song != null) {
 				try {
 					totalTime.setText(track.getTotalTime(song));
@@ -302,7 +304,7 @@ public class FXMLController extends StreamPlayer implements Initializable, Strea
 					showAlert("Warning", null, "The song failed to calculate the total play time!", AlertType.WARNING);
 				}
 				currentlyPlaying.setAlignment(Pos.CENTER);
-				currentlyPlaying.setText(artist+" ~ "+title);
+				currentlyPlaying.setText(artist + " ~ " + title);
 				try {
 					coverArt.setImage(new Image(new FileInputStream(art)));
 				} catch (NullPointerException e) {
@@ -316,12 +318,10 @@ public class FXMLController extends StreamPlayer implements Initializable, Strea
 				}
 				playTrack.setImage(new Image("icons/pause.png"));
 			}
-		}
-		else if (this.getStatus() == Status.PLAYING) {
+		} else if (this.getStatus() == Status.PLAYING) {
 			pause();
 			playTrack.setImage(new Image("icons/play.png"));
-		}
-		else if (this.getStatus() == Status.PAUSED) {
+		} else if (this.getStatus() == Status.PAUSED) {
 			resume();
 			playTrack.setImage(new Image("icons/pause.png"));
 		}
@@ -329,7 +329,7 @@ public class FXMLController extends StreamPlayer implements Initializable, Strea
 
 	public void nextSong(MouseEvent next) throws ClassNotFoundException, IOException {
 		int value = 0;
-		if (currentItem < addedSongs.size()-1) {
+		if (currentItem < addedSongs.size() - 1) {
 			stop();
 			value = currentItem + 1;
 			currentItem++;
@@ -340,8 +340,7 @@ public class FXMLController extends StreamPlayer implements Initializable, Strea
 			genre = temp.getGenre();
 			title = temp.getTitle();
 			playSong(next);
-		}
-		else {
+		} else {
 			showAlert("Information", null, "You have reached the end of the song list!", AlertType.INFORMATION);
 		}
 	}
@@ -359,8 +358,7 @@ public class FXMLController extends StreamPlayer implements Initializable, Strea
 			genre = temp.getGenre();
 			title = temp.getTitle();
 			playSong(prev);
-		}
-		else {
+		} else {
 			showAlert("Information", null, "You have reached the end of the song list!", AlertType.INFORMATION);
 		}
 	}
@@ -415,12 +413,12 @@ public class FXMLController extends StreamPlayer implements Initializable, Strea
 		saveDB();
 		System.exit(0);
 	}
-	
-	//MediaPlayer Overrides
+
+	// MediaPlayer Overrides
 	@Override
 	public void opened(Object arg0, Map<String, Object> arg1) {
 		setGain(volumeLevel);
-		
+
 		volumeSlider.valueProperty().addListener(new InvalidationListener() {
 			public void invalidated(Observable ov) {
 				if (volumeSlider.isValueChanging()) {
@@ -447,9 +445,10 @@ public class FXMLController extends StreamPlayer implements Initializable, Strea
 			}
 		});
 	}
-	
+
 	@Override
-	public void statusUpdated(StreamPlayerEvent arg0) {}
+	public void statusUpdated(StreamPlayerEvent arg0) {
+	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -458,6 +457,7 @@ public class FXMLController extends StreamPlayer implements Initializable, Strea
 			titleValue.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getTitle()));
 			artistValue.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getArtist()));
 			genreValue.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getGenre()));
-		} catch (NullPointerException e) {}
+		} catch (NullPointerException e) {
+		}
 	}
 }
